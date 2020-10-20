6525f153b1f87d85d8a8447c197d1ef53fd6b51c
Marcel Valdez <marcel.valdez@live.com>
2020-10-20 05:12:09 -0700

    Update default app_config

--- app_config.py
+++ app_config.py
@@ -1,11 +1,23 @@
-# This value is obtained by running scanmem on the tibia process
+""""These values are obtained by running scanmem on the tibia process."""
 MEM_CONFIG = {
-  # PID of Tibia process
-  'pid': 3400,
-  'mana_memory_address': '548c050',  # [I32 I16 ]
-  'speed_memory_address':  'e955c80',  # [I16 ]
-  'soul_points_memory_address': '452af58',
-  # The HP memory address can be reliably calculated based on the
-  # mana memory address.
-  'hp_memory_address': None  # [I32 I16 ]
+    # PID of Tibia process
+    'default_pid': 15922,
+    '14591': {
+        'mana_memory_address': '4350480',  # [I32 I16 ]
+        'speed_memory_address': 'b722ec0',  # [I16 ]
+        'soul_points_memory_address': '3718608',
+        # The HP memory address can be reliably calculated based on the
+        # mana memory address.
+        # Not required, since it can be calculated.
+        'hp_memory_address': None  # [I32 I16 ]
+    },
+    '21921': {
+        'mana_memory_address': '44b5670',  # [I32 I16 ]
+        'speed_memory_address': '79d9410',  # [I16 ]
+        'soul_points_memory_address': '4814588',
+        # The HP memory address can be reliably calculated based on the
+        # mana memory address.
+        # Not required, since it can be calculated.
+        'hp_memory_address': None  # [I32 I16 ]
+    }
 }
