655caaa411b3fa8364b3c19971bec6d19462b961
Marcel Valdez <marcel.valdez@live.com>
2021-02-09 10:24:54 -0800

    Make char_config use the example config

--- char_config.py
+++ char_config.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3.8
 
-import char_configs.demnok_char_config as demnok
-import char_configs.ultimate_char_config as ultimate
-import char_configs.tezalor_char_config as tezalor
-import char_configs.saekade_char_config as saekade
-import char_configs.imfereal_char_config as imfereal
-import char_configs.dio_sempai_config as dio_sempai
+import char_configs.example_char_config as example
 
 # To get the min average heal of your character use:
 # http://www.tibia-stats.com/index.php?akcja=spellDmgCalc
-
 
 
 HOTKEYS_CONFIG = {
@@ -23,7 +17,9 @@ HOTKEYS_CONFIG = {
     'eat_food': 'F8',
     'magic_shield': 'F9',
     'cancel_magic_shield': 'F10',
-    'loot': 'v'
+    'loot': 'v',
+    'toggle_emergency_amulet': 'F1',
+    'toggle_emergency_ring': 'F2',
 }
 
 # Whenever you want to switch characters, you have to specify
@@ -32,67 +28,7 @@ HOTKEYS_CONFIG = {
 # character configuration.
 CHAR_CONFIGS = [
     {
-        "name": "tezalor.tezalor_th_softboots_roh()",
-        "config": tezalor.tezalor_th_softboots_roh()
-    },
-    {
-        "name": "tezalor.tezalor_th_defensive()",
-        "config": tezalor.tezalor_th_defensive()
-    },
-    {
-        "name": "tezalor.UTAMO_TEAM",
-        "config": tezalor.UTAMO_TEAM
-    },
-    {
-        "name": "tezalor.ISSAVI_UTAMO",
-        "config": tezalor.ISSAVI_UTAMO
-    },
-    {
-        "name": "tezalor.UTAMO_SOLO_RUN",
-        "config": tezalor.UTAMO_SOLO_RUN
-    },
-    {
-        "name": "tezalor.MOBS_NO_UTAMO",
-        "config": tezalor.MOBS_NO_UTAMO,
-    },
-    {
-        "name": "tezalor.HARDCORE_MOBS_NO_UTAMO",
-        "config": tezalor.HARDCORE_MOBS_NO_UTAMO
-    },
-    {
-        "name": "demnok.ISSAVI_UTAMO",
-        "config": demnok.ISSAVI_UTAMO
-    },
-    {
-        "name": "demnok.ISSAVI_NO_UTAMO",
-        "config": demnok.ISSAVI_NO_UTAMO
-    },
-    {
-        "name": "demnok.SOLO_HUNT",
-        "config": demnok.SOLO_HUNT
-    },
-    {
-        "name": "imfereal.DEFAULT",
-        "config": imfereal.DEFAULT
-    },
-    {
-        "name": "saekade.MOBS_NO_UTAMO",
-        "config": saekade.MOBS_NO_UTAMO
-    },
-    {
-        "name": "saekade.ISSAVI",
-        "config": saekade.ISSAVI
-    },
-    {
-        "name": "ultimate.TEAMHUNT",
-        "config": ultimate.TEAMHUNT
-    },
-    {
-        "name": "dio_sempai.SOLO_HUNT_MOBS",
-        "config": dio_sempai.SOLO_HUNT_MOBS
-    },
-    {
-        "name": "dio_sempai.ISSAVI",
-        "config": dio_sempai.ISSAVI
+        "name": "example.DEFAULT",
+        "config": example.DEFAULT
     }
 ]
