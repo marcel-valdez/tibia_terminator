98eb74e873f010a998ebb3c63478b3e16f58e46a
Marcel Valdez <marcel.valdez@live.com>
2021-03-11 10:19:14 -0800

    Add future and lazy evaluation constructs

--- lazy_evaluator.py
+++ lazy_evaluator.py
@@ -31,24 +31,54 @@ class LazyValueNotReadyError(Exception):
 
 
 M = TypeVar("M")
-class LazyValue(Generic[M]):
+class FutureValue(Generic[M]):
     def __init__(self, getter: Callable[[], M]):
         self.lazy_evaluator = LazyEvaluator(getter, self.__done_callback)
         self.value_ready_event = Event()
-        self.lock = Lock()
 
     def __done_callback(self, value: M, error: Exception):
         self.value = value
         self.error = error
         self.value_ready_event.set()
 
-    def __start_evaluator(self):
-        if (not self.value_ready_event.is_set() and
-                not self.lazy_evaluator.is_alive()):
+    def start(self):
+        """Starts fetching the value asynchronously."""
+        if not self.value_ready_event.is_set() and not self.lazy_evaluator.is_alive():
+            self.lazy_evaluator.start()
+        return self
+
+    def __get(self) -> M:
+        if self.error is not None:
+            raise self.error
+        else:
+            return self.value
+
+    def get(self, timeout_sec=None) -> M:
+        if not self.value_ready_event.is_set():
+            if not self.value_ready_event.wait(timeout_sec):
+                raise LazyValueNotReadyError()
+
+        return self.__get()
+
+M = TypeVar("M")
+class LazyValue(Generic[M]):
+    def __init__(self, getter: Callable[[], M]):
+        self.getter = getter
+        self.value_ready_event = Event()
+        self.value = None
+        self.error = None
+        self.lock = Lock()
+
+    def __evaluate(self):
+        if not self.value_ready_event.is_set():
             self.lock.acquire()
-            if (not self.value_ready_event.is_set() and
-                    not self.lazy_evaluator.is_alive()):
-                self.lazy_evaluator.start()
+            if not self.value_ready_event.is_set():
+                try:
+                    self.value = self.getter()
+                except Exception as e:
+                    self.error = e
+                finally:
+                    self.value_ready_event.set()
             self.lock.release()
 
     def __get(self) -> M:
@@ -59,8 +89,17 @@ class LazyValue(Generic[M]):
 
     def get(self, timeout_sec=None) -> M:
         if not self.value_ready_event.is_set():
-            self.__start_evaluator()
+            self.__evaluate()
             if not self.value_ready_event.wait(timeout_sec):
                 raise LazyValueNotReadyError()
 
         return self.__get()
+
+K = TypeVar("K")
+def lazy(getter: Callable[[], K]) -> LazyValue[K]:
+    return LazyValue(getter)
+
+def future(getter: Callable[[], K]) -> FutureValue[K]:
+    future_value = FutureValue(getter)
+    future_value.start()
+    return future_value
--- test_lazy_evaluator.py
+++ test_lazy_evaluator.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3.8
 
 import unittest
-
-from unittest import (TestCase)
-from lazy_evaluator import LazyValue
-
-
 import time
 
+from unittest import (TestCase)
+from lazy_evaluator import LazyValue, FutureValue
+
+
+
+def get_value(time_ms=10) -> int:
+    time.sleep(time_ms / 1000)
+    return time.time() * 1000
 
 class TestLazyEvaluator(TestCase):
-    def test_get_value(self):
+    def test_get(self):
         # given
-        target = LazyValue(lambda: self.get_value(10))
+        target = LazyValue(lambda: get_value(10))
         # when
         millis = time.time() * 1000
         actual = target.get()
         # then
         self.assertGreater(actual, millis)
 
-    def test_get_value_twice(self):
+    def test_get_twice(self):
         # given
-        target = LazyValue(lambda: self.get_value(10))
+        target = LazyValue(lambda: get_value(10))
         expected = target.get()
         # when
         actual = target.get()
@@ -34,9 +37,34 @@ class TestLazyEvaluator(TestCase):
         # when
         self.assertRaises(ZeroDivisionError, target.get)
 
-    def get_value(self, time_ms=10) -> int:
-        time.sleep(time_ms / 1000)
-        return time.time() * 1000
+
+class TestFutureValue(TestCase):
+    def test_get(self):
+        # given
+        target = FutureValue(lambda: get_value(10))
+        millis = time.time() * 1000
+        target.start()
+        # when
+        actual = target.get()
+        # then
+        self.assertGreater(actual, millis)
+
+    def test_get_twice(self):
+        # given
+        target = FutureValue(lambda: get_value(10))
+        target.start()
+        expected = target.get()
+        # when
+        actual = target.get()
+        # then
+        self.assertEqual(actual, expected)
+
+    def test_get_error(self):
+        # given
+        target = FutureValue(lambda: 1/0)
+        target.start()
+        # when
+        self.assertRaises(ZeroDivisionError, target.get)
 
 
 if __name__ == '__main__':
