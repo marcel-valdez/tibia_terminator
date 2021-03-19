042e817db9cd11535931fa4281e6556b426e29cb
Marcel Valdez <marcel.valdez@live.com>
2021-03-19 11:45:11 -0700

    Add script to make it easy to execute tibia_terminator

---
+++ scripts/tibia_terminator.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ROOT_PATH="$(dirname ${SCRIPTPATH})/tibia_terminator"
+PYTHONPATH="${PYTHONPATH}:${ROOT_PATH}"
+RECONNECTOR_BIN="${ROOT_PATH}/main.py"
+
+function python_bin {
+  PYTHONPATH=${PYTHONPATH} python3.8 "$@"
+}
+
+python_bin ${RECONNECTOR_BIN} "$@"
