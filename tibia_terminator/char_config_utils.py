37d076e86aa09146fb9ab341802a4d483cc61fad
Marcel Valdez <_marcel_valdez_@live.com>
2021-12-10 13:58:30 -0800

    Remove obsolete char config example and utils

--- tibia_terminator/char_config_utils.py
+++
@@ -1,8 +0,0 @@
-"""Utilities for more easily creating char configs."""
-
-
-def copy(original, update):
-    """Make a copy of the original with updated values."""
-    copy = original.copy()
-    copy.update(update)
-    return copy
