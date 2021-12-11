9a7994141825a9d64d45d2837c9fa77b7ab063e6
Marcel Valdez <_marcel_valdez_@live.com>
2021-12-10 19:05:41 -0800

    Fix typo in emegency magic shield keeper (LOL\!)

--- tibia_terminator/keeper/emergency_magic_shield_keeper.py
+++ tibia_terminator/keeper/emergency_magic_shield_keeper.py
@@ -13,16 +13,16 @@ class EmergencyMagicShieldKeeper(MagicShieldKeeper):
                  emergency_reporter: EmergencyReporter,
                  tank_mode_reporter: TankModeReporter,
                  total_hp: int,
-                 magic_shield_treshold: int,
+                 magic_shield_threshold: int,
                  time_fn: Callable[[], int] = None):
-        super().__init__(client, total_hp, magic_shield_treshold, time_fn)
+        super().__init__(client, total_hp, magic_shield_threshold, time_fn)
         self.emergency_reporter = emergency_reporter
         self.tank_mode_reporter = tank_mode_reporter
 
     def should_cast(self, char_status):
         return ((self.emergency_reporter.is_in_emergency()
                  or self.tank_mode_reporter.is_tank_mode_on()) and
-                (char_status.magic_shield_level <= self.magic_shield_treshold
+                (char_status.magic_shield_level <= self.magic_shield_threshold
                  or self.secs_since_cast() >= MAGIC_SHIELD_DURATION_SECS - 10))
 
     def should_cast_cancel(self, char_status):
